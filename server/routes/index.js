3dac7d0f21561608407da622f279a8b3517bf0b5
Raul Pichardo <raul022107@gmail.com>
2023-01-05 16:59:25 -0400

    Adding initial files

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
  res.status(200).send("Hellow");
 });
 
+router.get('/ping', function(req, res, next) {
+  res.status(200).send("pong");
+});
+
 module.exports = router;
