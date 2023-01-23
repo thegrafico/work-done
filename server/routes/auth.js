80dbcec375e0c1a94e28be27b2f36e53c1915031
Raul Pichardo <raul@intelligentsystemsupport.com>
2023-01-23 17:26:32 -0400

    Adding Global error handle

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,37 +4,45 @@ const auth = require("../utils/auth");
 const UserCollection = require("../db/schema/user");
 const _ = require("lodash");
 
+const waitHanlder = function (req, res, next) {
 
-router.post("/login", async function (req, res, next) {
-  const {username, password} = req.body;
+}
 
-  if (!_.isString(username) || _.isEmpty(username) || !_.isString(password) || _.isEmpty(password)){
-    res.status(400).send({message:"Invalid usernamer or password"});
-    return;
-  }
-  console.log(username, password);
-  let error = null;
-  const user = await UserCollection.findUserByCredentials(username, password).catch(err => {
-    console.error("Error getting user: ", err);
-    error = err;
-  });
+router.post("/login", waitHanlder, async function (req, res, next) {
+  const { username, password } = req.body;
 
-  if (error) { 
-    res.status(500).send({message: "Oops, there was an error getting the user. Please try again"});
-    return;
-  }
+  setTimeout(async () => {
 
-  if (!user) {
-    res.status(403).send({message: "invalid credentials. User not found."});
-    return;
-  }
-  res.status(200).send(user);
+
+    if (!_.isString(username) || _.isEmpty(username) || !_.isString(password) || _.isEmpty(password)) {
+      res.status(400).send({ message: "Invalid usernamer or password" });
+      return;
+    }
+    console.log(username, password);
+    let error = null;
+    const user = await UserCollection.findUserByCredentials(username, password).catch(err => {
+      console.error("Error getting user: ", err);
+      error = err;
+    });
+
+    if (error) {
+      res.status(500).send({ message: "Oops, there was an error getting the user. Please try again" });
+      return;
+    }
+
+    if (!user) {
+      res.status(403).send({ message: "invalid credentials. User not found." });
+      return;
+    }
+    res.status(200).send(user);
+  }, 6000)
+
 });
 
 // TODO: Finish this
 router.get("/signin", async function (req, res, next) {
 
-  const nUser = await UserCollection.create({name: "raul", username: "rauleldomi", password: "admin"});
+  const nUser = await UserCollection.create({ name: "raul", username: "rauleldomi", password: "admin" });
   console.log("User created: ", nUser);
 
   res.status(200).send("Created dummy user");
