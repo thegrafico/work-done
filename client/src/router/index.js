46e7e83730cdf053d0d0b9a1c7ce964aa264e743
Raul Pichardo <raul022107@gmail.com>
2023-02-20 07:30:48 -0400

    Added initial template for user profile page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,44 +1,48 @@
-import { useAuthStore } from '@/stores/auth.store';
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { useAuthStore } from "@/stores/auth.store";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import AuthView from '../views/AuthView.vue';
-import NotFound from '../components/notFound/NotFound';
+import AuthView from "../views/AuthView.vue";
+import NotFound from "../components/notFound/NotFound";
 
 const routes = [
   {
-    path: '/login',
-    name: 'login',
-    component: AuthView
+    path: "/login",
+    name: "login",
+    component: AuthView,
   },
   {
-    path: '/dashboard',
-    name: 'dashboard',
-    alias: '/',
-    component: () => import('../views/DashboardView.vue') // lazy load
+    path: "/dashboard",
+    name: "dashboard",
+    alias: "/",
+    component: () => import("../views/DashboardView.vue"), // lazy load
   },
   {
-    path: '/project/:id/:tab',
-    name: 'project',
-    props: route => ({id: route.params.id, tab: route.params.tab}),
-    component: () => import('../views/ProjectView.vue') // lazy load
+    path: "/profile",
+    name: "profile",
+    component: () => import("../views/UserProfile.vue"), // lazy load
   },
   {
-    path: '/logout',
-    name: 'logout',
-    component: () => import('../views/LogoutView.vue') // lazy load
+    path: "/project/:id/:tab",
+    name: "project",
+    props: (route) => ({ id: route.params.id, tab: route.params.tab }),
+    component: () => import("../views/ProjectView.vue"), // lazy load
   },
-  { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound },
-]
+  {
+    path: "/logout",
+    name: "logout",
+    component: () => import("../views/LogoutView.vue"), // lazy load
+  },
+  { path: "/:pathMatch(.*)*", name: "not-found", component: NotFound },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 });
 
 router.beforeEach(async (to) => {
   checkIfUserIsLoggedIn(to);
 });
-
 
 // check if user is logged in and if not redirect to login page
 const checkIfUserIsLoggedIn = (to) => {
@@ -53,6 +57,6 @@ const checkIfUserIsLoggedIn = (to) => {
     auth.returnUrl = to.fullPath;
     auth.logout();
   }
-}
+};
 
 export default router;
---
+++ client/src/views/UserProfile.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app id="inspire">
+    <!-- Header -->
+    <Header title="Work Done" />
+
+    <!-- Main Container -->
+    <v-main>
+      <v-container width="80" class="myBG mt-10 pa-10" style="border-radius: 1%;">
+        <v-row>
+          <v-col align="center">
+            <v-avatar color="black" size="80"> <span class="text-h4 font-weight-bold"> RA </span></v-avatar>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col align="center">
+            {Username}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col align="center">
+            {password}
+            {confirm password}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col align="center">
+            {allow project invitations}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col align="center">
+            <v-btn color="primary">save</v-btn>
+          </v-col>
+        </v-row>
+
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+// Components
+import Header from "@/components/layout/HeaderView.vue";
+
+// Dynamic load views - Main section
+
+// TODO: getting username for the header. Probably is better to have this inside the component
+</script>
+
+<style scoped>
+.myBG {
+  background-color: #14171907;
+  max-height: 80vh;
+}
+</style>
